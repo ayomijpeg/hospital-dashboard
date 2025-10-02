525957597453134acf54579196eb4561a6438171
ayomijpeg <ayomitomiwa53@gmail.com>
2025-10-02 20:32:09 +0100

    Depoly

--- src/libs/supabase.js
+++ src/libs/supabase.js
@@ -1,10 +1,20 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://ntgrodtzkdcgiapzfwiq.supabase.co'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY
-console.log('Supabase Key:', process.env.NEXT_PUBLIC_SUPABASE_KEY);
 
+// Add better error handling
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error('Missing Supabase environment variables')
+}
 
-const supabase = createClient(supabaseUrl, supabaseKey)
+const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: true,
+    flowType: 'pkce'
+  }
+})
 
 export default supabase
